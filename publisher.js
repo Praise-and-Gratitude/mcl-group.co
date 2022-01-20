0cc3bcd929e2e1d60f95acbac9203d882eae89d6
Praise-and-Gratitude <ardirakhmat97@gmail.com>
2022-01-20 11:48:36 +0700

    first commit

---
+++ publisher.js
@@ -0,0 +1,243 @@
+//Cipta Kerja
+let moreCiptaKerja = document.getElementById('moreCiptaKerja')
+let fullCiptaKerja = document.getElementById('fullCiptaKerja')
+let lessCiptaKerja = document.getElementById('lessCiptaKerja')
+
+function readmoreCiptaKerja(){
+    fullCiptaKerja.style.display = 'block'
+    moreCiptaKerja.style.visibility = "hidden";
+    lessCiptaKerja.style.visibility = "visible";
+}
+
+function showlessCiptaKerja(){
+    fullCiptaKerja.style.display = 'none'
+    lessCiptaKerja.style.visibility = "hidden";
+    moreCiptaKerja.style.visibility = "visible";
+}
+moreCiptaKerja.addEventListener('click', readmoreCiptaKerja)
+lessCiptaKerja.addEventListener('click', showlessCiptaKerja) 
+
+
+//Misi
+let moreMisi = document.getElementById('moreMisi')
+let fullMisi = document.getElementById('fullMisi')
+let lessMisi = document.getElementById('lessMisi')
+
+function readmoreMisi(){
+    fullMisi.style.display = 'block'
+    moreMisi.style.visibility = "hidden";
+    lessMisi.style.visibility = "visible";
+}
+
+function showlessMisi(){
+    fullMisi.style.display = 'none'
+    lessMisi.style.visibility = "hidden";
+    moreMisi.style.visibility = "visible";
+}
+moreMisi.addEventListener('click', readmoreMisi)
+lessMisi.addEventListener('click', showlessMisi) 
+
+//PYKB
+let morePYKB = document.getElementById('morePYKB')
+let fullPYKB = document.getElementById('fullPYKB')
+let lessPYKB = document.getElementById('lessPYKB')
+
+function readmorePYKB(){
+    fullPYKB.style.display = 'block'
+    morePYKB.style.visibility = "hidden";
+    lessPYKB.style.visibility = "visible";
+}
+
+function showlessPYKB(){
+    fullPYKB.style.display = 'none'
+    lessPYKB.style.visibility = "hidden";
+    morePYKB.style.visibility = "visible";
+}
+morePYKB.addEventListener('click', readmorePYKB)
+lessPYKB.addEventListener('click', showlessPYKB) 
+
+
+//Bino
+let moreBino = document.getElementById('moreBino')
+let fullBino = document.getElementById('fullBino')
+let lessBino = document.getElementById('lessBino')
+
+function readmoreBino(){
+    fullBino.style.display = 'block'
+    moreBino.style.visibility = "hidden";
+    lessBino.style.visibility = "visible";
+}
+
+function showlessBino(){
+    fullBino.style.display = 'none'
+    lessBino.style.visibility = "hidden";
+    moreBino.style.visibility = "visible";
+}
+moreBino.addEventListener('click', readmoreBino)
+lessBino.addEventListener('click', showlessBino) 
+
+//Dayon
+let moredayon = document.getElementById('moredayon')
+let fulldayon = document.getElementById('fulldayon')
+let lessdayon = document.getElementById('lessdayon')
+
+function readmoredayon(){
+    fulldayon.style.display = 'block'
+    moredayon.style.visibility = "hidden";
+    lessdayon.style.visibility = "visible";
+}
+
+function showlessdayon(){
+    fulldayon.style.display = 'none'
+    lessdayon.style.visibility = "hidden";
+    moredayon.style.visibility = "visible";
+}
+moredayon.addEventListener('click', readmoredayon)
+lessdayon.addEventListener('click', showlessdayon) 
+
+
+//PTBMKTP
+let morePTBMKTP = document.getElementById('morePTBMKTP')
+let fullPTBMKTP = document.getElementById('fullPTBMKTP')
+let lessPTBMKTP = document.getElementById('lessPTBMKTP')
+
+function readmorePTBMKTP(){
+    fullPTBMKTP.style.display = 'block'
+    morePTBMKTP.style.visibility = "hidden";
+    lessPTBMKTP.style.visibility = "visible";
+}
+
+function showlessPTBMKTP(){
+    fullPTBMKTP.style.display = 'none'
+    lessPTBMKTP.style.visibility = "hidden";
+    morePTBMKTP.style.visibility = "visible";
+}
+morePTBMKTP.addEventListener('click', readmorePTBMKTP)
+lessPTBMKTP.addEventListener('click', showlessPTBMKTP) 
+
+//Revisi MKPSD
+let moreRevisiMKPSD = document.getElementById('moreRevisiMKPSD')
+let fullRevisiMKPSD = document.getElementById('fullRevisiMKPSD')
+let lessRevisiMKPSD = document.getElementById('lessRevisiMKPSD')
+
+function readmoreRevisiMKPSD(){
+    fullRevisiMKPSD.style.display = 'block'
+    moreRevisiMKPSD.style.visibility = "hidden";
+    lessRevisiMKPSD.style.visibility = "visible";
+}
+
+function showlessRevisiMKPSD(){
+    fullRevisiMKPSD.style.display = 'none'
+    lessRevisiMKPSD.style.visibility = "hidden";
+    moreRevisiMKPSD.style.visibility = "visible";
+}
+moreRevisiMKPSD.addEventListener('click', readmoreRevisiMKPSD)
+lessRevisiMKPSD.addEventListener('click', showlessRevisiMKPSD) 
+
+
+//MKPSD
+let moreMKPSD = document.getElementById('moreMKPSD')
+let fullMKPSD = document.getElementById('fullMKPSD')
+let lessMKPSD = document.getElementById('lessMKPSD')
+
+function readmoreMKPSD(){
+    fullMKPSD.style.display = 'block'
+    moreMKPSD.style.visibility = "hidden";
+    lessMKPSD.style.visibility = "visible";
+}
+
+function showlessMKPSD(){
+    fullMKPSD.style.display = 'none'
+    lessMKPSD.style.visibility = "hidden";
+    moreMKPSD.style.visibility = "visible";
+}
+moreMKPSD.addEventListener('click', readmoreMKPSD)
+lessMKPSD.addEventListener('click', showlessMKPSD) 
+
+
+//Deli
+let moredeli = document.getElementById('moredeli')
+let fulldeli = document.getElementById('fulldeli')
+let lessdeli = document.getElementById('lessdeli')
+
+function readmoredeli(){
+    fulldeli.style.display = 'block'
+    moredeli.style.visibility = "hidden";
+    lessdeli.style.visibility = "visible";
+}
+
+function showlessdeli(){
+    fulldeli.style.display = 'none'
+    lessdeli.style.visibility = "hidden";
+    moredeli.style.visibility = "visible";
+}
+moredeli.addEventListener('click', readmoredeli)
+lessdeli.addEventListener('click', showlessdeli) 
+
+
+//PPS
+let morePPS = document.getElementById('morePPS')
+let fullPPS = document.getElementById('fullPPS')
+let lessPPS = document.getElementById('lessPPS')
+
+function readmorePPS(){
+    fullPPS.style.display = 'block'
+    morePPS.style.visibility = "hidden";
+    lessPPS.style.visibility = "visible";
+}
+
+function showlessPPS(){
+    fullPPS.style.display = 'none'
+    lessPPS.style.visibility = "hidden";
+    morePPS.style.visibility = "visible";
+}
+morePPS.addEventListener('click', readmorePPS)
+lessPPS.addEventListener('click', showlessPPS) 
+
+
+//Siri
+let moresiri = document.getElementById('moresiri')
+let fullsiri = document.getElementById('fullsiri')
+let lesssiri = document.getElementById('lesssiri')
+
+function readmoresiri(){
+    fullsiri.style.display = 'block'
+    moresiri.style.visibility = "hidden";
+    lesssiri.style.visibility = "visible";
+}
+
+function showlesssiri(){
+    fullsiri.style.display = 'none'
+    lesssiri.style.visibility = "hidden";
+    moresiri.style.visibility = "visible";
+}
+moresiri.addEventListener('click', readmoresiri)
+lesssiri.addEventListener('click', showlesssiri) 
+
+
+//Load More
+
+let firstMore = document.getElementById('firstMore')
+let secondMore = document.getElementById('secondMore')
+let thirdMore = document.getElementById('thirdMore')
+let thirdBookRow = document.getElementById('thirdBookRow')
+let secondBookRow = document.getElementById('secondBookRow')
+let firstBookRow = document.getElementById('firstBookRow')
+
+
+function firstMoreButton(){
+    firstMore.style.display = 'none'
+    secondMore.style.display = 'block'
+    thirdBookRow.style.display = 'flex'
+    secondBookRow.style.display = 'flex'
+}
+
+function secondMoreButton(){
+    secondMore.style.display = 'none'
+    //thirdMore.style.display = 'block'
+    firstBookRow.style.display = 'flex'
+}
+
+firstMore.addEventListener('click', firstMoreButton)
+secondMore.addEventListener('click', secondMoreButton)
+
