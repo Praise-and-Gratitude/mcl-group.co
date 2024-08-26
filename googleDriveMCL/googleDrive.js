882c20c3d74c1e296cf2868e4bef0a1f7751d872
Ardi Rakhmat <65388076+Praise-and-Gratitude@users.noreply.github.com>
2024-08-26 16:22:27 +0700

    Add files via upload

---
+++ googleDriveMCL/googleDrive.js
@@ -0,0 +1,651 @@
+//Reformulasi Uang Pengganti
+let moreReformulasiUangPengganti = document.getElementById('moreReformulasiUangPengganti')
+let fullReformulasiUangPengganti = document.getElementById('fullReformulasiUangPengganti')
+
+function readmoreReformulasiUangPengganti(){
+    fullReformulasiUangPengganti.style.display = 'block'
+    moreReformulasiUangPengganti.style.visibility = "hidden";
+
+}
+
+function showlessReformulasiUangPengganti(){
+    fullReformulasiUangPengganti.style.display = 'none'
+    moreReformulasiUangPengganti.style.visibility = "visible";
+}
+
+moreReformulasiUangPengganti.addEventListener('click', readmoreReformulasiUangPengganti)
+
+
+//Kita Dan Kata
+let moreKitaDanKata = document.getElementById('moreKitaDanKata')
+let fullKitaDanKata = document.getElementById('fullKitaDanKata')
+
+function readmoreKitaDanKata(){
+    fullKitaDanKata.style.display = 'block'
+    moreKitaDanKata.style.visibility = "hidden";
+}
+
+function showlessKitaDanKata(){
+    fullKitaDanKata.style.display = 'none'
+    moreKitaDanKata.style.visibility = "visible";
+}
+
+moreKitaDanKata.addEventListener('click', readmoreKitaDanKata)
+
+
+//KUHP2
+let moreKUHP2 = document.getElementById('moreKUHP2')
+let fullKUHP2 = document.getElementById('fullKUHP2')
+
+
+function readmoreKUHP2(){
+    fullKUHP2.style.display = 'block'
+    moreKUHP2.style.visibility = "hidden";
+    
+}
+
+function showlessKUHP2(){
+    fullKUHP2.style.display = 'none'
+    moreKUHP2.style.visibility = "visible";
+}
+moreKUHP2.addEventListener('click', readmoreKUHP2)
+
+
+
+//Kita Di Titik Temu
+let moreKitaDiTitikTemu = document.getElementById('moreKitaDiTitikTemu')
+let fullKitaDiTitikTemu = document.getElementById('fullKitaDiTitikTemu')
+
+
+function readmoreKitaDiTitikTemu(){
+    fullKitaDiTitikTemu.style.display = 'block'
+    moreKitaDiTitikTemu.style.visibility = "hidden";
+    
+}
+
+function showlessKitaDiTitikTemu(){
+    fullKitaDiTitikTemu.style.display = 'none'
+    moreKitaDiTitikTemu.style.visibility = "visible";
+}
+
+moreKitaDiTitikTemu.addEventListener('click', readmoreKitaDiTitikTemu)
+
+
+//Sang Penceramah
+let moreSangPenceramah = document.getElementById('moreSangPenceramah')
+let fullSangPenceramah = document.getElementById('fullSangPenceramah')
+
+
+function readmoreSangPenceramah(){
+    fullSangPenceramah.style.display = 'block'
+    moreSangPenceramah.style.visibility = "hidden";
+    
+}
+
+function showlessSangPenceramah(){
+    fullSangPenceramah.style.display = 'none'
+    moreSangPenceramah.style.visibility = "visible";
+}
+moreSangPenceramah.addEventListener('click', readmoreSangPenceramah)
+
+
+//Tindak Pidana Cipta Kerja
+let moreTindakPidanaCiptaKerja = document.getElementById('moreTindakPidanaCiptaKerja')
+let fullTindakPidanaCiptaKerja = document.getElementById('fullTindakPidanaCiptaKerja')
+
+
+function readmoreTindakPidanaCiptaKerja(){
+    fullTindakPidanaCiptaKerja.style.display = 'block'
+    moreTindakPidanaCiptaKerja.style.visibility = "hidden";
+    
+}
+
+function showlessTindakPidanaCiptaKerja(){
+    fullTindakPidanaCiptaKerja.style.display = 'none'
+    moreTindakPidanaCiptaKerja.style.visibility = "visible";
+}
+moreTindakPidanaCiptaKerja.addEventListener('click', readmoreTindakPidanaCiptaKerja)
+
+
+//POV
+let morePOV = document.getElementById('morePOV')
+let fullPOV = document.getElementById('fullPOV')
+
+
+function readmorePOV(){
+    fullPOV.style.display = 'block'
+    morePOV.style.visibility = "hidden";
+    
+}
+
+function showlessPOV(){
+    fullPOV.style.display = 'none'
+    morePOV.style.visibility = "visible";
+}
+morePOV.addEventListener('click', readmorePOV)
+
+
+//KUHP
+let moreKUHP = document.getElementById('moreKUHP')
+let fullKUHP = document.getElementById('fullKUHP')
+
+
+function readmoreKUHP(){
+    fullKUHP.style.display = 'block'
+    moreKUHP.style.visibility = "hidden";
+
+}
+
+function showlessKUHP(){
+    fullKUHP.style.display = 'none'
+    moreKUHP.style.visibility = "visible";
+}
+moreKUHP.addEventListener('click', readmoreKUHP)
+
+
+//Helium Mengelilingi Kita
+let moreHeliumMengelilingiKita = document.getElementById('moreHeliumMengelilingiKita')
+let fullHeliumMengelilingiKita = document.getElementById('fullHeliumMengelilingiKita')
+
+
+function readmoreHeliumMengelilingiKita(){
+    fullHeliumMengelilingiKita.style.display = 'block'
+    moreHeliumMengelilingiKita.style.visibility = "hidden";
+    
+}
+
+function showlessHeliumMengelilingiKita(){
+    fullHeliumMengelilingiKita.style.display = 'none'
+    moreHeliumMengelilingiKita.style.visibility = "visible";
+}
+moreHeliumMengelilingiKita.addEventListener('click', readmoreHeliumMengelilingiKita)
+
+
+//Bara Dalam Sekar
+let moreBaraDalamSekar = document.getElementById('moreBaraDalamSekar')
+let fullBaraDalamSekar = document.getElementById('fullBaraDalamSekar')
+
+
+function readmoreBaraDalamSekar(){
+    fullBaraDalamSekar.style.display = 'block'
+    moreBaraDalamSekar.style.visibility = "hidden";
+    
+}
+
+function showlessBaraDalamSekar(){
+    fullBaraDalamSekar.style.display = 'none'
+    moreBaraDalamSekar.style.visibility = "visible";
+}
+moreBaraDalamSekar.addEventListener('click', readmoreBaraDalamSekar)
+
+
+//Damba Lara Cinta
+let moreDambaLaraCinta = document.getElementById('moreDambaLaraCinta')
+let fullDambaLaraCinta = document.getElementById('fullDambaLaraCinta')
+
+
+function readmoreDambaLaraCinta(){
+    fullDambaLaraCinta.style.display = 'block'
+    moreDambaLaraCinta.style.visibility = "hidden";
+    
+}
+
+function showlessDambaLaraCinta(){
+    fullDambaLaraCinta.style.display = 'none'
+    lessDambaLaraCinta.style.visibility = "hidden";
+}
+moreDambaLaraCinta.addEventListener('click', readmoreDambaLaraCinta)
+
+//Reaktualisasi Poros Maritim
+let moreReaktualisasiPorosMaritim = document.getElementById('moreReaktualisasiPorosMaritim')
+let fullReaktualisasiPorosMaritim = document.getElementById('fullReaktualisasiPorosMaritim')
+
+function readmoreReaktualisasiPorosMaritim(){
+    fullReaktualisasiPorosMaritim.style.display = 'block'
+    moreReaktualisasiPorosMaritim.style.visibility = "hidden";
+}
+
+function showlessReaktualisasiPorosMaritim(){
+    fullReaktualisasiPorosMaritim.style.display = 'none'
+    moreReaktualisasiPorosMaritim.style.visibility = "visible";
+}
+moreReaktualisasiPorosMaritim.addEventListener('click', readmoreReaktualisasiPorosMaritim)
+
+//Nuansa Perdata
+let moreNuansaPerdata = document.getElementById('moreNuansaPerdata')
+let fullNuansaPerdata = document.getElementById('fullNuansaPerdata')
+
+function readmoreNuansaPerdata(){
+    fullNuansaPerdata.style.display = 'block'
+    moreNuansaPerdata.style.visibility = "hidden";
+}
+
+function showlessNuansaPerdata(){
+    fullNuansaPerdata.style.display = 'none'
+    moreNuansaPerdata.style.visibility = "visible";
+}
+moreNuansaPerdata.addEventListener('click', readmoreNuansaPerdata)
+
+
+//Kerugian Keuangan Negara
+let moreKerugianKeuanganNegara = document.getElementById('moreKerugianKeuanganNegara')
+let fullKerugianKeuanganNegara = document.getElementById('fullKerugianKeuanganNegara')
+
+function readmoreKerugianKeuanganNegara(){
+    fullKerugianKeuanganNegara.style.display = 'block'
+    moreKerugianKeuanganNegara.style.visibility = "hidden";
+}
+
+function showlessKerugianKeuanganNegara(){
+    fullKerugianKeuanganNegara.style.display = 'none'
+    moreKerugianKeuanganNegara.style.visibility = "visible";
+}
+moreKerugianKeuanganNegara.addEventListener('click', readmoreKerugianKeuanganNegara)
+
+//Mendirikan Salat Menegakan Peradaban
+let moreMendirikanSalat = document.getElementById('moreMendirikanSalat')
+let fullMendirikanSalat = document.getElementById('fullMendirikanSalat')
+
+function readmoreMendirikanSalat(){
+    fullMendirikanSalat.style.display = 'block'
+    moreMendirikanSalat.style.visibility = "hidden";
+}
+
+function showlessMendirikanSalat(){
+    fullMendirikanSalat.style.display = 'none'
+    moreMendirikanSalat.style.visibility = "visible";
+}
+moreMendirikanSalat.addEventListener('click', readmoreMendirikanSalat)
+
+//RahasiaGemini
+let moreRahasiaGemini = document.getElementById('moreRahasiaGemini')
+let fullRahasiaGemini = document.getElementById('fullRahasiaGemini')
+
+function readmoreRahasiaGemini(){
+    fullRahasiaGemini.style.display = 'block'
+    moreRahasiaGemini.style.visibility = "hidden";
+}
+
+function showlessRahasiaGemini(){
+    fullRahasiaGemini.style.display = 'none'
+    moreRahasiaGemini.style.visibility = "visible";
+}
+moreRahasiaGemini.addEventListener('click', readmoreRahasiaGemini)
+ 
+ //Elegi
+let moreElegi = document.getElementById('moreElegi')
+let fullElegi = document.getElementById('fullElegi')
+
+function readmoreElegi(){
+    fullElegi.style.display = 'block'
+    moreElegi.style.visibility = "hidden";
+}
+
+function showlessElegi(){
+    fullElegi.style.display = 'none'
+    moreElegi.style.visibility = "visible";
+}
+moreElegi.addEventListener('click', readmoreElegi)
+ 
+ //Buku Kuning
+ let moreBukuKuning = document.getElementById('moreBukuKuning')
+ let fullBukuKuning = document.getElementById('fullBukuKuning')
+ 
+ function readmoreBukuKuning(){
+     fullBukuKuning.style.display = 'block'
+     moreBukuKuning.style.visibility = "hidden";
+ }
+ 
+ function showlessBukuKuning(){
+     fullBukuKuning.style.display = 'none'
+     moreBukuKuning.style.visibility = "visible";
+ }
+ moreBukuKuning.addEventListener('click', readmoreBukuKuning)
+
+//Sembilan Bintang Farmasi
+let moresembilanBintangFarmasi = document.getElementById('moresembilanBintangFarmasi')
+let fullsembilanBintangFarmasi = document.getElementById('fullsembilanBintangFarmasi')
+
+function readmoresembilanBintangFarmasi(){
+    fullsembilanBintangFarmasi.style.display = 'block'
+    moresembilanBintangFarmasi.style.visibility = "hidden";
+}
+
+function showlesssembilanBintangFarmasi(){
+    fullsembilanBintangFarmasi.style.display = 'none'
+    moresembilanBintangFarmasi.style.visibility = "visible";
+}
+moresembilanBintangFarmasi.addEventListener('click', readmoresembilanBintangFarmasi)
+
+
+//KHSKR
+let moreKHSKR = document.getElementById('moreKHSKR')
+let fullKHSKR = document.getElementById('fullKHSKR')
+
+function readmoreKHSKR(){
+    fullKHSKR.style.display = 'block'
+    moreKHSKR.style.visibility = "hidden";
+}
+
+function showlessKHSKR(){
+    fullKHSKR.style.display = 'none'
+    moreKHSKR.style.visibility = "visible";
+}
+moreKHSKR.addEventListener('click', readmoreKHSKR)
+
+//Gagule
+let moreGagule = document.getElementById('moreGagule')
+let fullGagule = document.getElementById('fullGagule')
+
+function readmoreGagule(){
+    fullGagule.style.display = 'block'
+    moreGagule.style.visibility = "hidden";
+}
+
+function showlessGagule(){
+    fullGagule.style.display = 'none'
+    moreGagule.style.visibility = "visible";
+}
+moreGagule.addEventListener('click', readmoreGagule)
+
+//Sabai
+let moreSabai = document.getElementById('moreSabai')
+let fullSabai = document.getElementById('fullSabai')
+
+function readmoreSabai(){
+    fullSabai.style.display = 'block'
+    moreSabai.style.visibility = "hidden";
+}
+
+function showlessSabai(){
+    fullSabai.style.display = 'none'
+    moreSabai.style.visibility = "visible";
+}
+moreSabai.addEventListener('click', readmoreSabai)
+
+//LYN
+let moreLYN = document.getElementById('moreLYN')
+let fullLYN = document.getElementById('fullLYN')
+
+function readmoreLYN(){
+    fullLYN.style.display = 'block'
+    moreLYN.style.visibility = "hidden";
+}
+
+function showlessLYN(){
+    fullLYN.style.display = 'none'
+    moreLYN.style.visibility = "visible";
+}
+moreLYN.addEventListener('click', readmoreLYN)
+
+
+//Dimensi Langit Manusia
+let moreDLM = document.getElementById('moreDLM')
+let fullDLM = document.getElementById('fullDLM')
+
+function readmoreDLM(){
+    fullDLM.style.display = 'block'
+    moreDLM.style.visibility = "hidden";
+}
+
+function showlessDLM(){
+    fullDLM.style.display = 'none'
+    moreDLM.style.visibility = "visible";
+}
+moreDLM.addEventListener('click', readmoreDLM)
+
+
+//Cipta Kerja
+let moreCiptaKerja = document.getElementById('moreCiptaKerja')
+let fullCiptaKerja = document.getElementById('fullCiptaKerja')
+
+function readmoreCiptaKerja(){
+    fullCiptaKerja.style.display = 'block'
+    moreCiptaKerja.style.visibility = "hidden";
+}
+
+function showlessCiptaKerja(){
+    fullCiptaKerja.style.display = 'none'
+    moreCiptaKerja.style.visibility = "visible";
+}
+moreCiptaKerja.addEventListener('click', readmoreCiptaKerja)
+
+
+//Misi
+let moreMisi = document.getElementById('moreMisi')
+let fullMisi = document.getElementById('fullMisi')
+
+function readmoreMisi(){
+    fullMisi.style.display = 'block'
+    moreMisi.style.visibility = "hidden";
+}
+
+function showlessMisi(){
+    fullMisi.style.display = 'none'
+    moreMisi.style.visibility = "visible";
+}
+moreMisi.addEventListener('click', readmoreMisi)
+
+//PYKB
+let morePYKB = document.getElementById('morePYKB')
+let fullPYKB = document.getElementById('fullPYKB')
+
+function readmorePYKB(){
+    fullPYKB.style.display = 'block'
+    morePYKB.style.visibility = "hidden";
+}
+
+function showlessPYKB(){
+    fullPYKB.style.display = 'none'
+    morePYKB.style.visibility = "visible";
+}
+morePYKB.addEventListener('click', readmorePYKB)
+
+
+//Bino
+let moreBino = document.getElementById('moreBino')
+let fullBino = document.getElementById('fullBino')
+
+function readmoreBino(){
+    fullBino.style.display = 'block'
+    moreBino.style.visibility = "hidden";
+}
+
+function showlessBino(){
+    fullBino.style.display = 'none'
+    moreBino.style.visibility = "visible";
+}
+moreBino.addEventListener('click', readmoreBino)
+
+//Dayon
+let moredayon = document.getElementById('moredayon')
+let fulldayon = document.getElementById('fulldayon')
+
+function readmoredayon(){
+    fulldayon.style.display = 'block'
+    moredayon.style.visibility = "hidden";
+}
+
+function showlessdayon(){
+    fulldayon.style.display = 'none'
+    moredayon.style.visibility = "visible";
+}
+moredayon.addEventListener('click', readmoredayon)
+
+
+//PTBMKTP
+let morePTBMKTP = document.getElementById('morePTBMKTP')
+let fullPTBMKTP = document.getElementById('fullPTBMKTP')
+
+function readmorePTBMKTP(){
+    fullPTBMKTP.style.display = 'block'
+    morePTBMKTP.style.visibility = "hidden";
+}
+
+function showlessPTBMKTP(){
+    fullPTBMKTP.style.display = 'none'
+    morePTBMKTP.style.visibility = "visible";
+}
+morePTBMKTP.addEventListener('click', readmorePTBMKTP)
+
+//Revisi MKPSD
+let moreRevisiMKPSD = document.getElementById('moreRevisiMKPSD')
+let fullRevisiMKPSD = document.getElementById('fullRevisiMKPSD')
+
+function readmoreRevisiMKPSD(){
+    fullRevisiMKPSD.style.display = 'block'
+    moreRevisiMKPSD.style.visibility = "hidden";
+}
+
+function showlessRevisiMKPSD(){
+    fullRevisiMKPSD.style.display = 'none'
+    moreRevisiMKPSD.style.visibility = "visible";
+}
+moreRevisiMKPSD.addEventListener('click', readmoreRevisiMKPSD)
+
+
+//MKPSD
+let moreMKPSD = document.getElementById('moreMKPSD')
+let fullMKPSD = document.getElementById('fullMKPSD')
+
+function readmoreMKPSD(){
+    fullMKPSD.style.display = 'block'
+    moreMKPSD.style.visibility = "hidden";
+}
+
+function showlessMKPSD(){
+    fullMKPSD.style.display = 'none'
+    moreMKPSD.style.visibility = "visible";
+}
+moreMKPSD.addEventListener('click', readmoreMKPSD)
+
+
+//Deli
+let moredeli = document.getElementById('moredeli')
+let fulldeli = document.getElementById('fulldeli')
+
+function readmoredeli(){
+    fulldeli.style.display = 'block'
+    moredeli.style.visibility = "hidden";
+}
+
+function showlessdeli(){
+    fulldeli.style.display = 'none'
+    moredeli.style.visibility = "visible";
+}
+moredeli.addEventListener('click', readmoredeli)
+
+
+//PPS
+let morePPS = document.getElementById('morePPS')
+let fullPPS = document.getElementById('fullPPS')
+
+function readmorePPS(){
+    fullPPS.style.display = 'block'
+    morePPS.style.visibility = "hidden";
+}
+
+function showlessPPS(){
+    fullPPS.style.display = 'none'
+    morePPS.style.visibility = "visible";
+}
+morePPS.addEventListener('click', readmorePPS)
+
+
+//Siri
+let moresiri = document.getElementById('moresiri')
+let fullsiri = document.getElementById('fullsiri')
+
+function readmoresiri(){
+    fullsiri.style.display = 'block'
+    moresiri.style.visibility = "hidden";
+}
+
+function showlesssiri(){
+    fullsiri.style.display = 'none'
+    moresiri.style.visibility = "visible";
+}
+moresiri.addEventListener('click', readmoresiri)
+
+
+//Load More
+
+let firstMore = document.getElementById('firstMore')
+let secondMore = document.getElementById('secondMore')
+let thirdMore = document.getElementById('thirdMore')
+let fourthMore= document.getElementById('fourthMore')
+let fifthMore= document.getElementById('fifthMore')
+let sixthMore= document.getElementById('sixthMore')
+
+
+let firstBookRow = document.getElementById('firstBookRow')
+let secondBookRow = document.getElementById('secondBookRow')
+let thirdBookRow = document.getElementById('thirdBookRow')
+let fourthBookRow = document.getElementById('fourthBookRow')
+let fifthBookRow = document.getElementById('fifthBookRow')
+let sixthBookRow = document.getElementById('sixthBookRow')
+let seventhBookRow = document.getElementById('seventhBookRow')
+let eighthBookRow = document.getElementById('eighthBookRow')
+let ninthBookRow = document.getElementById('ninthBookRow')
+let tenthBookRow = document.getElementById('tenthBookRow')
+let eleventhBookRow = document.getElementById('eleventhBookRow')
+let twelfthBookRow = document.getElementById('twelfthBookRow')
+let thirteenthBookRow = document.getElementById('thirteenthBookRow')
+let fourteenthBookRow = document.getElementById('fourteenthBookRow')
+let fifteenthBookRow = document.getElementById('fifteenthBookRow')
+let sixteenthBookRow = document.getElementById('sixteenthBookRow')
+let seventeenthBookRow = document.getElementById('seventeenthBookRow')
+let eightteenthBookRow = document.getElementById('eightteenthBookRow')
+
+function sixthMoreButton(){
+    sixthMore.style.display = 'none'
+    fifthMore.style.display = 'block'
+    fifteenthBookRow.style.display = 'flex'
+    fourteenthBookRow.style.display = 'flex'
+}
+
+
+function fifthMoreButton(){
+    fifthMore.style.display = 'none'
+    fourthMore.style.display = 'block'
+    thirteenthBookRow.style.display = 'flex'
+    twelfthBookRow.style.display = 'flex'
+}
+
+function fourthMoreButton(){
+    fourthMore.style.display = 'none'
+    thirdMore.style.display = 'block'
+    eleventhBookRow.style.display = 'flex'
+    tenthBookRow.style.display = 'flex'
+    ninthBookRow.style.display = 'flex'
+}
+
+function thirdMoreButton(){
+    thirdMore.style.display = 'none'
+    secondMore.style.display = 'block'
+    eighthBookRow.style.display = 'flex'
+    seventhBookRow.style.display = 'flex'
+}
+
+function secondMoreButton(){
+    secondMore.style.display = 'none'
+    firstMore.style.display = 'block'
+    sixthBookRow.style.display = 'flex'
+    fifthBookRow.style.display = 'flex'
+    fourthBookRow.style.display = 'flex'
+}
+
+function firstMoreButton(){
+    firstMore.style.display = 'none'
+    thirdBookRow.style.display = 'flex'
+    secondBookRow.style.display = 'flex'
+    firstBookRow.style.display = 'flex'
+}
+
+firstMore.addEventListener('click', firstMoreButton)
+secondMore.addEventListener('click', secondMoreButton)
+thirdMore.addEventListener('click', thirdMoreButton)
+fourthMore.addEventListener('click',fourthMoreButton)
+fifthMore.addEventListener('click',fifthMoreButton)
+sixthMore.addEventListener('click',sixthMoreButton)
+
